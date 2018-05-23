d02d0c0783f5b4298e1b1997513570eb110aa554
PikalaxALT <pikalaxalt@gmail.com>
2018-05-22 22:20:06 -0400

    fix pikahelp glitch

--- utils/checks.py
+++ utils/checks.py
@@ -3,8 +3,9 @@ import discord
 from discord.ext import commands
 
 
-async def ctx_is_owner(ctx):
+async def ctx_is_owner(ctx: commands.Context):
     if await ctx.bot.is_owner(ctx.author):
         return True
-    await ctx.send(f'{ctx.author.mention}: Permission denied')
+    if ctx.command.name != 'pikahelp':
+        await ctx.send(f'{ctx.author.mention}: Permission denied')
     return False
