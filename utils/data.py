7c3ba19a6d9927b7fc03e0b5d791f966c703c5a7
PikalaxALT <pikalaxalt@gmail.com>
2018-05-29 22:41:16 -0400

    Implement yolonome and refactor data

--- utils/data.py
+++ utils/data.py
@@ -1,7 +1,31 @@
+import csv
+import random
+
+
 class Data:
     def __init__(self):
-        with open('data/pokemon.txt') as fp:
-            self.pokemon = tuple(line.strip() for line in fp)
+        with open('data/pokemon.tsv') as fp:
+            self.pokemon = list(csv.DictReader(fp))
+        with open('data/moves.tsv') as fp:
+            self.moves = list(csv.DictReader(fp))
+
+    def random_pokemon(self):
+        return random.choice(self.pokemon)
+
+    def random_pokemon_attr(self, attr, default=None):
+        return self.random_pokemon().get(attr, default)
+
+    def random_move(self):
+        return random.choice(self.moves)
+
+    def random_move_attr(self, attr, default=None):
+        return self.random_move().get(attr, default)
+
+    def random_pokemon_name(self):
+        return self.random_pokemon_attr('name', 'Phancero')
+
+    def random_move_name(self):
+        return self.random_move_attr('name', 'Struggle')
 
 
 data = Data()
