bb9a96d1d89118bf95b7080149f9f553f5f13eeb
PikalaxALT <pikalaxalt@gmail.com>
2018-07-10 00:17:46 -0500

    Self-assignable rolees

---
+++ cogs/self_role.py
@@ -0,0 +1,64 @@
+import discord
+from discord.ext import commands
+from utils.default_cog import Cog
+
+
+class SelfAssignableRole(Cog):
+    roles = {}
+    config_attrs = 'roles',
+
+    async def __error(self, ctx: commands.Context, exc: BaseException):
+        pass
+
+    @commands.command()
+    @commands.bot_has_permissions(manage_roles=True)
+    async def iam(self, ctx: commands.Context, role: discord.Role):
+        if role.id not in self.roles.get(ctx.guild.id, []):
+            await ctx.send(f'Role "{role}" is not self-assignable')
+        elif role in ctx.author.roles:
+            await ctx.send(f'You already have role "{role}"')
+        else:
+            await ctx.author.add_roles(role, reason='Requested by user')
+            await ctx.send(f'You now have the role "{role}"')
+
+    @commands.command()
+    @commands.bot_has_permissions(manage_roles=True)
+    async def iamnot(self, ctx: commands.Context, role: discord.Role):
+        if role.id not in self.roles.get(ctx.guild.id, []):
+            await ctx.send(f'Role "{role}" is not self-assignable')
+        elif role not in ctx.author.roles:
+            await ctx.send(f'You don\'t have the role "{role}"')
+        else:
+            await ctx.author.remove_roles(role, reason='Requested by user')
+            await ctx.send(f'You no longer have the role "{role}"')
+
+    @commands.command()
+    @commands.is_owner()
+    async def addar(self, ctx: commands.Context, role: discord.Role):
+        if ctx.guild.id not in self.roles:
+            self.roles[ctx.guild.id] = [role.id]
+        elif role.id in self.roles[ctx.guild.id]:
+            return await ctx.send(f'Role "{role}" already self-assignable"')
+        else:
+            self.roles[ctx.guild.id].append(role.id)
+        await ctx.send(f'Role "{role}" is now self-assignable')
+
+    @commands.command()
+    @commands.is_owner()
+    async def rmar(self, ctx: commands.Context, role: discord.Role):
+        if ctx.guild.id not in self.roles:
+            self.roles[ctx.guild.id] = []
+        if role.id not in self.roles[ctx.guild.id]:
+            return await ctx.send(f'Role "{role}" is self-assignable"')
+        self.roles[ctx.guild.id].remove(role.id)
+        await ctx.send(f'Role "{role}" is no longer self-assignable')
+
+    @commands.command()
+    async def lsar(self, ctx):
+        roles = ', '.join(str(discord.utils.get(ctx.guild.roles, id=role)) for role in self.roles[ctx.guild.id])
+        await ctx.send(f'Self-assignable roles for {ctx.guild}:\n'
+                       f'{roles}')
+
+
+def setup(bot):
+    bot.add_cog(SelfAssignableRole(bot))
