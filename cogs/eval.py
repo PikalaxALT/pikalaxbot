185ef8289f83194f6b04bae9f87ac80eeaff87ef
PikalaxALT <pikalaxalt@gmail.com>
2018-08-02 14:18:40 -0400

    fix to shell cmd, 7

--- cogs/eval.py
+++ cogs/eval.py
@@ -151,13 +151,15 @@ class Eval(Cog):
             color = discord.Color.red() if returncode != 0 or exc else discord.Color.green()
             title = f'Process exited with status code {returncode}' if returncode is not None else 'Process timed out'
             embed = discord.Embed(title=title, color=color)
-            stdout_f = await self.format_embed_value(embed, 'stdout', stdout.decode())
-            stderr_f = await self.format_embed_value(embed, 'stderr', stderr.decode())
+            files = [
+                await self.format_embed_value(embed, 'stdout', stdout.decode()),
+                await self.format_embed_value(embed, 'stderr', stderr.decode())
+            ]
             if exc:
                 tb = ''.join(traceback.format_exception(type(exc), exc, exc.__traceback__))
-                tb_f = await self.format_embed_value(embed, 'traceback', tb)
+                files.append(await self.format_embed_value(embed, 'traceback', tb))
             await ctx.send(embed=embed)
-            for fl in (stdout_f, stderr_f, tb_f):
+            for fl in files:
                 if fl is not None:
                     await ctx.send(file=fl)
 
