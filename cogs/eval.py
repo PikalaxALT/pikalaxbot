e0968cb73717ab67b32febe3ec7d23642e0386e1
PikalaxALT <pikalaxalt@gmail.com>
2018-08-03 09:43:49 -0400

    Use similar output format for eval and shell, 3

--- cogs/eval.py
+++ cogs/eval.py
@@ -117,9 +117,8 @@ class Eval(ClientSessionCog):
         body = self.cleanup_code(body)
         stdout = io.StringIO()
 
-        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
-
         try:
+            to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
             exec(to_compile, env)
         except Exception as e:
             return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
