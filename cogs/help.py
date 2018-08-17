492ff8dec0e683d1fd7658f189396fc64f17c5f0
PikalaxALT <pikalaxalt@gmail.com>
2018-08-17 10:03:42 -0400

    Fuck it, adding a global command error listener overriding the bot-level listener

--- cogs/help.py
+++ cogs/help.py
@@ -35,6 +35,9 @@ class Help(BaseCog):
             exc = exc.orig
         await ctx.send(exc)
 
+    async def on_command_error(self, ctx, exc):
+        await self.help_error(ctx, exc)
+
 
 def setup(bot):
     help_name = bot.help_attrs['name']
