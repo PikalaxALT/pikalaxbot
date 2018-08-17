0d8443018aa93fab0dc3e01c41b18bc9caf8f77d
PikalaxALT <pikalaxalt@gmail.com>
2018-08-17 12:18:08 -0400

    This is hacky and should not be a thing but Python

--- cogs/help.py
+++ cogs/help.py
@@ -4,40 +4,34 @@ from cogs import BaseCog
 from utils.paginator import HelpPaginator
 
 
-class Help(BaseCog):
-    def __init__(self, bot):
-        super().__init__(bot)
-        self.help_cmd = commands.command(**bot.help_attrs)(self._help)
-        self.help_cmd.error(self.help_error)
-        self.help_cmd.instance = self
-
-    async def _help(self, ctx: commands.Context, *, command=None):
-        """Shows help about a command or the bot"""
-        print('User invoked the help command owo')
-
-        if command is None:
-            p = await HelpPaginator.from_bot(ctx)
-        else:
-            entity = self.bot.get_cog(command) or self.bot.get_command(command)
-
-            if entity is None:
-                clean = command.replace('@', '@\u200b')
-                return await ctx.send(f'Command or category "{clean}" not found.')
-            elif isinstance(entity, commands.Command):
-                p = await HelpPaginator.from_command(ctx, entity)
-            else:
-                p = await HelpPaginator.from_cog(ctx, entity)
-
-        await p.paginate()
-
-    async def help_error(self, ctx, exc):
-        self.log_tb(ctx, exc)
-        if hasattr(exc, 'orig'):
-            exc = exc.orig
-        await ctx.send(exc)
-
-
 def setup(bot):
+    class Help(BaseCog):
+        @commands.command(**bot.help_attrs)
+        async def _help(self, ctx: commands.Context, *, command=None):
+            """Shows help about a command or the bot"""
+            print('User invoked the help command owo')
+
+            if command is None:
+                p = await HelpPaginator.from_bot(ctx)
+            else:
+                entity = self.bot.get_cog(command) or self.bot.get_command(command)
+
+                if entity is None:
+                    clean = command.replace('@', '@\u200b')
+                    return await ctx.send(f'Command or category "{clean}" not found.')
+                elif isinstance(entity, commands.Command):
+                    p = await HelpPaginator.from_command(ctx, entity)
+                else:
+                    p = await HelpPaginator.from_cog(ctx, entity)
+
+            await p.paginate()
+
+        async def help_error(self, ctx, exc):
+            self.log_tb(ctx, exc)
+            if hasattr(exc, 'orig'):
+                exc = exc.orig
+            await ctx.send(exc)
+
     help_name = bot.help_attrs['name']
     bot.remove_command(help_name)
     bot.add_cog(Help(bot))
