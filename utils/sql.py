0fd0cbc4aed75b3edee50984a9e03a8869783883
PikalaxALT <pikalaxalt@gmail.com>
2018-08-28 17:32:17 -0400

    async sqlite3, 9

--- utils/sql.py
+++ utils/sql.py
@@ -43,6 +43,7 @@ class Sql:
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         await self._connection.commit()
+        await self._connection.close()
 
     async def execute(self, script, args=()):
         return await self._connection.execute(script, args)
