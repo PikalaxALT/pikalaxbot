6edef3507288cb71144b50fdbb9a9920bf338dee
PikalaxALT <pikalaxalt@gmail.com>
2018-09-07 15:29:51 -0400

    edit aiosqlite requirement

---
+++ utils/ircbot.py
@@ -0,0 +1,16 @@
+import asyncio
+from irc.client import Connection, Event
+from irc.bot import SingleServerIRCBot, ServerSpec
+from .botclass import LoggingMixin
+
+
+class AsyncIRCBot(SingleServerIRCBot, LoggingMixin):
+    def __init__(self, *args, loop=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.loop = loop or asyncio.get_event_loop()
+
+    async def start(self):
+        await self.loop.run_in_executor(None, super().start)
+
+    def on_privmsg(self, connection: Connection, event: Event):
+        self.log_debug(f'{event}')
