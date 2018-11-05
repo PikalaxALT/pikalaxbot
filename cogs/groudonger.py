fa115bc174a3cf43c71ee4de335c93ba8b6e1ec8
PikalaxALT <pikalaxalt@gmail.com>
2018-11-05 11:35:06 -0500

    Add license header to groudonger extension

--- cogs/groudonger.py
+++ cogs/groudonger.py
@@ -1,5 +1,20 @@
+# PikalaxBOT - A Discord bot in discord.py
+# Copyright (C) 2018  PikalaxALT
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import discord
-from discord.ext import commands
 from cogs import BaseCog
 from utils.botclass import PikalaxBOT
 
@@ -7,7 +22,7 @@ from utils.botclass import PikalaxBOT
 class Groudonger(BaseCog):
     async def on_reaction_add(self, reaction, user):
         msg: discord.Message = reaction.message
-        channel:discord.TextChannel = msg.channel
+        channel: discord.TextChannel = msg.channel
         guild: discord.Guild = msg.guild
 
         groudonger: discord.Member = guild.get_member(303257160421212160)
