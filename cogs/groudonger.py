8f120a5f985cfea515e13ebd9c0bf0ee29beaa40
PikalaxALT <pikalaxalt@gmail.com>
2018-11-06 07:52:01 -0500

    Round 2 FIGHT

--- cogs/groudonger.py
+++ cogs/groudonger.py
@@ -26,10 +26,11 @@ class Groudonger(BaseCog):
         guild: discord.Guild = msg.guild
 
         groudonger: discord.Member = guild.get_member(303257160421212160)
-        emoji: discord.Emoji = discord.utils.get(guild.emojis, id=507398431115837441)
+        cog = self.bot.get_cog('Markov')
 
-        if user == groudonger and reaction.emoji == emoji:
-            await channel.send('!wow')
+        if user == groudonger and msg.author == guild.me:
+            chain = cog.gen_msg(len_max=250, n_attempts=10)
+            await channel.send(f'!mail {chain}')
             await self.bot.wait_for('message', check=lambda m: m.author == groudonger and m.channel == channel)
             await channel.send(f'{groudonger.mention} pls')
 
