e42633f406af9ad8f602df205cc6d53284eb1d45
PikalaxALT <pikalaxalt@gmail.com>
2018-11-06 10:15:12 -0500

    add timeout to groudonger wait_for; require 2 options for choose

--- cogs/groudonger.py
+++ cogs/groudonger.py
@@ -31,7 +31,11 @@ class Groudonger(BaseCog):
         if user == groudonger and msg.author == guild.me:
             chain = cog.gen_msg(len_max=250, n_attempts=10)
             await channel.send(f'!mail {chain}')
-            await self.bot.wait_for('message', check=lambda m: m.author == groudonger and m.channel == channel)
+            await self.bot.wait_for(
+                'message',
+                check=lambda m: m.author == groudonger and m.channel == channel,
+                timeout=60
+            )
             await channel.send(f'{groudonger.mention} pls')
 
 
