3857e77820fd14a0deeda874b069f5e8d2ee95bf
PikalaxALT <pikalaxalt@gmail.com>
2019-11-29 13:33:04 -0500

    More robust run.sh, 3

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 BOTDIR=$(dirname "$(realpath -P "$0")"); cd "${BOTDIR}"
-pip install -U -r requirements.txt  # update discord and youtube-dl
+python3 -m pip install -U -r requirements.txt  # update discord and youtube-dl
 git pull  # update the bot
 python3 bot.py  # start the bot
