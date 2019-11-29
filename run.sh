15a097ef139d0c9f469120a3eee45862e2fe12b6
PikalaxALT <pikalaxalt@gmail.com>
2019-11-29 13:10:59 -0500

    Use system python3 interpreter without forcing minor version

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 cd /home/pi/pikalaxbot/
 pip install -U -r requirements.txt  # update discord and youtube-dl
 git pull  # update the bot
-python3.6 bot.py  # start the bot
+python3 bot.py  # start the bot
