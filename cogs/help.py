22ae8f57f76bf3f825cd8209a2ff670218cb03eb
PikalaxALT <pikalaxalt@gmail.com>
2019-11-29 13:06:34 -0500

    Update help command once more lol

--- cogs/help.py
+++ cogs/help.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 
 
-class MyHelpCommand(commands.HelpCommand):
+class MyHelpCommand(commands.DefaultHelpCommand):
     def get_command_signature(self, command):
         return '{0.clean_prefix}{1.qualified_name} {1.signature}'.format(self, command)
 
