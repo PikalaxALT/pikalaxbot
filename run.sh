143c98bc77e88aa978f8113fb8531524ae1b6da5
PikalaxALT <pikalaxalt@gmail.com>
2019-11-29 13:20:35 -0500

    More robust run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-cd /home/pi/pikalaxbot/
+which python3
+
+BOTDIR=$(dirname "$(realpath -P "$0")"); cd "${BOTDIR}"
 pip install -U -r requirements.txt  # update discord and youtube-dl
 git pull  # update the bot
 python3 bot.py  # start the bot
