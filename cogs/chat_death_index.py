6ce1731f4f5a885f2448f621b0e8543771b33b85
PikalaxALT <PikalaxALT@users.noreply.github.com>
2019-12-07 06:55:12 -0500

    Update chat_death_index.py

--- cogs/chat_death_index.py
+++ cogs/chat_death_index.py
@@ -110,11 +110,11 @@ class ChatDeathIndex(BaseCog):
         n = len(chat_avg)
         if n < self.MAX_SAMPLES:
             return await ctx.send(f'I cannot determine the Chat Death Index of {channel.mention} at this time.')
-        await ctx.typing()
-        filename = await self.bot.loop.run_in_executor(None, self.plot, self, channel)
-        if isinstance(filename, Exception):
-            raise filename
-        file = discord.File(filename)
+        async with ctx.typing()
+            filename = await self.bot.loop.run_in_executor(None, self.plot, channel)
+            if isinstance(filename, Exception):
+                raise filename
+            file = discord.File(filename)
         await ctx.send(file=file)
 
     @plot_cdi.error
