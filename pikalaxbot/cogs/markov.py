eeb40c010eb23b456b787102ee8d08462a094756
PikalaxALT <PikalaxALT@gmail.com>
2019-12-31 20:09:20 -0500

    Fix markov

--- pikalaxbot/cogs/markov.py
+++ pikalaxbot/cogs/markov.py
@@ -142,7 +142,7 @@ class Markov(BaseCog):
         self.learn_markov(msg)
         try:
             if await self.markov.can_run(ctx):
-                await ctx.invoke(self.markov, None)
+                await ctx.invoke(self.markov, recipient=None)
         except commands.CheckFailure:
             pass
 
