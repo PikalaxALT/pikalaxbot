a623b49c641c8f67bf58bf017dce4f28986793a2
PikalaxALT <PikalaxALT@gmail.com>
2020-01-05 16:33:35 -0500

    rotate xticklabels

--- pikalaxbot/cogs/ping.py
+++ pikalaxbot/cogs/ping.py
@@ -44,7 +44,7 @@ class Ping(BaseCog):
         plt.fill_between(range(history), [0 for _ in values], values)
         xtickvalues = list(range(0, history, history // 10 + (history % 10 != 0)))
         xticklabels = [(self.start_time + datetime.timedelta(seconds=i * 30)).strftime('%Y-%m-%dT%H:%M:%S') for i in xtickvalues]
-        plt.xticks(xtickvalues, xticklabels)
+        plt.xticks(xtickvalues, xticklabels, rotation='vertical')
         plt.xlabel('Time (UTC)')
         plt.ylabel('Heartbeat latency (ms)')
         plt.savefig(buffer)
