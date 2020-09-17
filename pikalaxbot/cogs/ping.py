55ae0dac38d2ad66823e1521cfa199aa7ce9316b
PikalaxALT <pikalaxalt@gmail.com>
2020-09-17 08:38:04 -0400

    Update ping history plotter for arbitrary times

--- pikalaxbot/cogs/ping.py
+++ pikalaxbot/cogs/ping.py
@@ -11,21 +11,19 @@ import matplotlib.pyplot as plt
 class Ping(BaseCog):
     def __init__(self, bot):
         super().__init__(bot)
-        self.ping_history = []
+        self.ping_history = {}
         self.build_ping_history.start()
-        self.start_time = None
 
     def cog_unload(self):
         self.build_ping_history.cancel()
 
     @tasks.loop(seconds=30)
     async def build_ping_history(self):
-        self.ping_history.append(self.bot.latency * 1000)
+        self.ping_history[datetime.datetime.utcnow()] = self.bot.latency * 1000
 
     @build_ping_history.before_loop
     async def before_ping_history(self):
         await self.bot.wait_until_ready()
-        self.start_time = datetime.datetime.utcnow()
 
     @commands.group(invoke_without_command=True)
     async def ping(self, ctx: commands.Context):
@@ -36,18 +34,16 @@ class Ping(BaseCog):
                                f'Heartbeat latency: {self.bot.latency * 1000:.0f} ms')
 
     def do_plot_ping(self, buffer, history):
-        values = self.ping_history
-        start_time = self.start_time
-        if history > 0:
-            start_time = max(start_time, start_time + datetime.timedelta(minutes=len(values) / 2 - history))
-            values = values[-2 * history:]
-        history = len(values)
+        start_time = datetime.datetime.utcnow() - datetime.timedelta(minutes=history)
+        times, values = zip(*sorted([t for t in self.ping_history.items() if t[0] >= start_time]))
         plt.figure()
-        plt.plot(range(history), values)
-        plt.fill_between(range(history), [0 for _ in values], values)
-        xtickvalues = list(range(0, history, history // 10 + (history % 10 != 0)))
-        xticklabels = [(start_time + datetime.timedelta(seconds=i * 30)).strftime('%Y-%m-%d\nT%H:%M:%S') for i in xtickvalues]
-        plt.xticks(xtickvalues, xticklabels, rotation=45, ha='right', ma='right')
+        ax: plt.Axes = plt.gca()
+        ax.plot(times, values)
+        ax.fill_between(times, [0 for _ in values], values)
+        labs = ax.get_xticklabels()
+        tick_width = (times[-1] - times[0]).total_seconds() / (len(labs) - 1)
+        new_labs = [(times[0] + datetime.timedelta(seconds=i * tick_width)).strftime('%Y-%m-%d\nT%H:%M:%S') for i in range(len(labs))]
+        ax.set_xticklabels(new_labs, rotation=45, ha='right', ma='right')
         plt.xlabel('Time (UTC)')
         plt.ylabel('Heartbeat latency (ms)')
         plt.tight_layout()
@@ -70,8 +66,8 @@ class Ping(BaseCog):
     async def dump_ping(self, ctx: commands.Context):
         async with ctx.typing():
             output = b''
-            for i, record in enumerate(self.ping_history):
-                timestamp = (self.start_time + datetime.timedelta(seconds=30 * i)).timestamp()
+            for t, record in self.ping_history.items():
+                timestamp = t.timestamp()
                 output += struct.pack('=dd', timestamp, record)
             curtime = datetime.datetime.utcnow().strftime('%Y%m%d.%H%M%S')
             with open(f'ping_{curtime}.bin', 'wb') as ofp:
