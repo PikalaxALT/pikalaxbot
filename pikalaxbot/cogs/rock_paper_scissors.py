9460026bc2612ca956d6ec284e2fb6eba69eb914
PikalaxALT <pikalaxalt@gmail.com>
2020-10-08 16:03:09 -0400

    Refactor RPS to subclass Menu

--- pikalaxbot/cogs/rock_paper_scissors.py
+++ pikalaxbot/cogs/rock_paper_scissors.py
@@ -7,11 +7,19 @@ from . import BaseCog
 _emojis = '\U0001faa8', '\U0001f4f0', '\u2702', '\u274c'
 
 
-class RockPaperScissors(BaseCog):
-    @commands.command(aliases=['rps'])
-    @commands.max_concurrency(1)
-    async def rock_paper_scissors(self, ctx: commands.Context):
-        """Play a game of Rock-Paper-Scissors"""
+class RPSMenu(menus.Menu):
+    async def my_action(self, payload):
+        self.player_move = _emojis.index(str(payload.emoji))
+        self.stop()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.player_move = 3
+        self.bot_move = random.randint(0, 2)
+        for emoji in _emojis:
+            self.add_button(menus.Button(emoji, self.my_action))
+
+    async def send_initial_message(self, ctx, channel):
         embed = discord.Embed(
             title=f'Rock Paper Scissors with {ctx.author}',
             description='Once all four reactions have been set, choose one to'
@@ -19,31 +27,33 @@ class RockPaperScissors(BaseCog):
                         ' React with \u274c to cancel.',
             colour=discord.Colour.orange()
         )
-        msg = await ctx.send(embed=embed)
-        menu = menus.Menu(message=msg, clear_reactions_after=True)
-        menu.player_move = 3
-        menu.bot_move = random.randint(0, 2)
+        return await channel.send(embed=embed)
 
-        async def reaction(menu_, payload):
-            menu_.player_move = _emojis.index(str(payload.emoji))
-            menu_.stop()
-
-        for emoji in _emojis:
-            menu.add_button(menus.Button(emoji, reaction))
-        await menu.start(ctx, wait=True)
-
-        if menu.player_move == 3:
+    async def finalize(self, timed_out):
+        embed = self.message.embeds[0]
+        if timed_out:
+            embed.description += '\n\nTime\'s up, the player left'
+        elif self.player_move == 3:
             embed.description += '\n\nGame cancelled by player'
         else:
-            player_emoji = _emojis[menu.player_move]
-            bot_emoji = _emojis[menu.bot_move]
-            diff = (menu.bot_move - menu.player_move) % 3
+            player_emoji = _emojis[self.player_move]
+            bot_emoji = _emojis[self.bot_move]
+            diff = (self.bot_move - self.player_move) % 3
             # 0: tied
             # 1: bot wins
             # 2: player wins
-            embed.description += f'\n\n{ctx.author}: {player_emoji}\n{self.bot.user}: {bot_emoji}\n\n'
+            embed.description += f'\n\n{self.ctx.author}: {player_emoji}\n{self.bot.user}: {bot_emoji}\n\n'
             embed.description += ('It\'s a draw!', 'The player loses...', 'The player wins!')[diff]
-        await msg.edit(embed=embed)
+        await self.message.edit(embed=embed)
+
+
+class RockPaperScissors(BaseCog):
+    @commands.command(aliases=['rps'])
+    @commands.max_concurrency(1)
+    async def rock_paper_scissors(self, ctx: commands.Context):
+        """Play a game of Rock-Paper-Scissors"""
+        menu = RPSMenu(clear_reactions_after=True)
+        await menu.start(ctx)
 
     async def cog_command_error(self, ctx, error):
         error = getattr(error, 'original', error)
