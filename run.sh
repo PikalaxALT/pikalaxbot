14b2678532e9e19c92fb8758a974fd87c7dacfd8
PikalaxALT <pikalaxalt@gmail.com>
2020-10-11 16:21:11 -0400

    jishaku no underscore, 2

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-export JISHAKU_NO_UNDERSCORE=true
-
 BOTDIR=$(dirname "$(realpath -P "$0")"); cd "${BOTDIR}"
 git pull  # update the bot
 python3 -m pip install -U -r requirements.txt  # update discord
-python3 -m pikalaxbot  # start the bot
+JISHAKU_NO_UNDERSCORE=true python3 -m pikalaxbot  # start the bot
