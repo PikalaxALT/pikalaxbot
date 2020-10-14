94f22230ea662a46c455b6fcdb70f960dc3fe5ad
PikalaxALT <pikalaxalt@gmail.com>
2020-10-14 08:02:57 -0400

    Refactor SeenUser

--- pikalaxbot/cogs/seen_user.py
+++ pikalaxbot/cogs/seen_user.py
@@ -1,20 +1,24 @@
 import datetime
 import discord
-from collections import defaultdict, namedtuple
+from collections import defaultdict
 from discord.ext import commands
 import typing
 from . import BaseCog
+from humanize import naturaldelta
+import re
 
 
-MessageProxy = namedtuple('MessageProxy', 'message_id author_id channel_id created_at')
+def get_msg_reference(message):
+    return discord.MessageReference(
+        None,  # message._state
+        message_id=message.id,
+        channel_id=message.channel.id,
+        guild_id=message.guild.id
+    )
 
 
-def proxy(message):
-    return MessageProxy(message.id, message.author.id, message.channel.id, message.created_at)
-
-
-def get_jump_url(ctx, proxy):
-    return f'https://discordapp.com/channels/{ctx.guild.id}/{proxy.channel_id}/{proxy.message_id}'
+def get_jump_url(ref):
+    return f'https://discordapp.com/channels/{ref.guild_id}/{ref.channel_id}/{ref.message_id}'
 
 
 class SeenUser(BaseCog):
@@ -28,26 +32,26 @@ class SeenUser(BaseCog):
     @BaseCog.listener()
     async def on_message(self, message: discord.Message):
         if message.guild is not None:
-            proxy_ = proxy(message)
-            self.member_cache[(message.guild.id, message.author.id)] = proxy_
-            self.history_cache[message.channel.id].append(proxy_)
+            ref = get_msg_reference(message)
+            self.member_cache[(message.guild.id, message.author.id)] = ref
+            self.history_cache[message.channel.id].append(ref)
 
-    async def get_last_seen_msg(self, member: discord.Member) -> typing.Optional[MessageProxy]:
+    async def get_last_seen_msg(self, member: discord.Member) -> typing.Optional[discord.MessageReference]:
         last = datetime.datetime.utcnow() - SeenUser.MAX_LOOKBACK
-        seen_msg: typing.Optional[MessageProxy] = None
+        seen_msg: typing.Optional[discord.MessageReference] = None
         for channel in member.guild.text_channels:  # type: discord.TextChannel
             if channel.id in self.history_cache:
                 history = self.history_cache[channel.id]
             elif channel.permissions_for(member.guild.me).read_message_history:
-                history = [proxy(message) async for message in channel.history(limit=None, after=last)]
+                history = [get_msg_reference(message) async for message in channel.history(limit=None, after=last)]
                 if history:
-                    history.sort(key=lambda msg: msg.created_at)
+                    history.sort(key=lambda msg: discord.utils.snowflake_time(msg.message_id))
                 self.history_cache[channel.id] = history
             else:
                 continue
             seen_msg = discord.utils.get(reversed(history), author_id=member.id) or seen_msg
             if seen_msg is not None:
-                last = seen_msg.created_at
+                last = discord.utils.snowflake_time(seen_msg.message_id)
         return seen_msg
 
     @commands.command()
@@ -61,10 +65,14 @@ class SeenUser(BaseCog):
             seen_msg = await self.get_last_seen_msg(member)
             self.member_cache[key] = seen_msg
         if seen_msg is None:
-            await ctx.send(f'{member.display_name} has not said anything on this server recently.')
+            ndelt = naturaldelta(SeenUser.MAX_LOOKBACK)
+            # 1 day is parsed to "a day" but that's bad grammar here
+            ndelt = re.sub(r'^an? ', '', ndelt)
+            await ctx.send(f'{member.display_name} has not said anything on this server in the last {ndelt}.')
         else:
             await ctx.send(f'{member.display_name} was last seen chatting in <#{seen_msg.channel_id}> '
-                           f'{seen_msg.created_at.strftime("on %d %B %Y at %H:%M:%S UTC")}\n{get_jump_url(ctx, seen_msg)}')
+                           f'{discord.utils.snowflake_time(seen_msg.message_id).strftime("on %d %B %Y at %H:%M:%S UTC")}\n'
+                           f'{get_jump_url(seen_msg)}')
 
 
 def setup(bot):
