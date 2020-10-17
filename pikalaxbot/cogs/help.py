72dce124fca44b77431ce297f0aa2a589dfb4f02
PikalaxALT <pikalaxalt@gmail.com>
2020-10-17 11:26:26 -0400

    Remove Danny's paginator

--- pikalaxbot/cogs/help.py
+++ pikalaxbot/cogs/help.py
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Credits to Danny for making this featureful paginated help.
+# 17 Oct 2020: Refactored to use ext.menus
 
 import discord
 from discord.ext import commands, menus
