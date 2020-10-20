85a4a1379fa59b0f259a39a5250688d3cff1c3ad
PikalaxALT <pikalaxalt@gmail.com>
2020-10-20 16:30:41 -0400

    Unfuck everything

--- pikalaxbot/cogs/modtools.py
+++ pikalaxbot/cogs/modtools.py
@@ -198,7 +198,10 @@ class Modtools(BaseCog):
             extension = f'pikalaxbot.cogs.{cog}'
         real_cog = cog.title().replace('_', '')
         try:
-            await self.bot.loop.run_in_executor(None, method, extension)
+            if extension == 'ext.pokeapi':
+                self.bot.loop.run_in_executor(None, method, extension)
+            else:
+                method(extension)
         except commands.ExtensionError:
             await ctx.send(f'Failed to {mode} cog "{real_cog}"')
             raise
