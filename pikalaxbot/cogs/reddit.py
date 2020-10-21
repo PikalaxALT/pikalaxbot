41f27b2b03221241724d97d4b97001823f93161f
PikalaxALT <pikalaxalt@gmail.com>
2020-10-20 20:25:16 -0400

    Fix reddit error

--- pikalaxbot/cogs/reddit.py
+++ pikalaxbot/cogs/reddit.py
@@ -14,6 +14,12 @@ with open(os.path.join(os.path.dirname(__dir__), 'version.txt')) as fp:
     __version__ = fp.read().strip()
 
 
+class SubredditNotFound(commands.CommandError):
+    def __init__(self, subreddit, message, *args):
+        super().__init__(message, *args)
+        self.subreddit = subreddit
+
+
 class Reddit(BaseCog):
     def __init__(self, bot):
         super().__init__(bot)
@@ -40,7 +46,7 @@ class Reddit(BaseCog):
             if child.get('url_overridden_by_dest') and not child.get('is_video') and not child.get('media'):
                 break
         else:
-            return await ctx.send(f'Hmm... I seem to be out of {subreddit} right now')
+            raise SubredditNotFound(subreddit)
         author = child['author']
         permalink = child['permalink']
         score = child['score']
@@ -75,6 +81,8 @@ class Reddit(BaseCog):
                 await ctx.send('I cannot find that subreddit!')
             else:
                 await ctx.send(f'An unhandled HTTP exception occurred: {error.status}: {error.message}')
+        elif isinstance(error, SubredditNotFound):
+            await ctx.send(f'Hmm... I seem to be out of {error.subreddit} right now')
         elif isinstance(error, commands.CheckFailure):
             pass
         else:
