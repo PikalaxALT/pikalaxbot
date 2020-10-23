49eec04fda78fc4874e387eec9f90b484344d8c2
PikalaxALT <pikalaxalt@gmail.com>
2020-10-23 14:19:01 -0400

    Sort hoisters by nickname

--- pikalaxbot/cogs/modtools.py
+++ pikalaxbot/cogs/modtools.py
@@ -373,6 +373,7 @@ class Modtools(BaseCog):
             if not any(role.hoist for role in member.roles)
             and member.display_name[0] < '0'
         ]
+        hoisters.sort(key=lambda m: m.display_name)
         menu = menus.MenuPages(HoisterPageSource(hoisters, per_page=1), delete_message_after=True)
         await menu.start(ctx, wait=True)
 
