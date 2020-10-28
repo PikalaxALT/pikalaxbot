229c580e8ed4f6c635bc3c43e89450c871d7ad58
PikalaxALT <pikalaxalt@gmail.com>
2020-10-28 09:15:45 -0400

    More lenient definition of hoisters

--- pikalaxbot/cogs/hoisters.py
+++ pikalaxbot/cogs/hoisters.py
@@ -56,7 +56,7 @@ class Hoisters(BaseCog):
             member for member in ctx.guild.members
             if not any(role.hoist for role in member.roles)
             and not member.bot  # bots are exempt
-            and member.display_name < '0'
+            and member.display_name < ' '
         ]
         hoisters.sort(key=lambda m: (m.nick is None, m.status is discord.Status.offline, m.display_name))
         menu = HoistersMenu(HoisterPageSource(hoisters, per_page=9), delete_message_after=True)
