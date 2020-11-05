55efd06e93bdc74ff7a79aff4ef0bc876ddfab1a
PikalaxALT <pikalaxalt@gmail.com>
2020-11-05 15:04:31 -0500

    charinfo command

---
+++ pikalaxbot/cogs/char_info.py
@@ -0,0 +1,57 @@
+import discord
+from discord.ext import commands, menus
+from . import BaseCog
+from .utils.menus import NavMenuPages
+import unicodedata
+
+
+class CharInfoMenu(menus.ListPageSource):
+    async def format_page(self, menu, page):
+        c, digit, name = page
+        c_ord = ord(c)
+        py_repr = f'{digit}'
+        if name != 'Name not found':
+            py_repr += f'\n\\N{{{name}}}'
+        return discord.Embed(
+            title=f'Info for character: {c}',
+            colour=discord.Colour.greyple()
+        ).add_field(
+            name='Codepoint',
+            value=f'{c_ord:x}'
+        ).add_field(
+            name='Python repr',
+            value=py_repr
+        ).add_field(
+            name='More info',
+            value=f'[Click here](http://www.fileformat.info/info/unicode/char/{digit}'
+        )
+
+
+class CharInfo(BaseCog):
+    @commands.command()
+    async def charinfo(self, ctx, *, characters):
+        """Shows you information about a number of characters."""
+
+        units = [
+            (0xFFFF0000, 8, 'U'),
+            (0xFF00, 4, 'u'),
+            (0xFF, 2, 'u'),
+        ]
+
+        def as_hex_str(c, default=None):
+            c_ord = ord(c)
+            for mask, width, code in units:
+                if c_ord & mask:
+                    return (f'\\{code}{{:0{width}x}}').format(c_ord)
+            if default is not None:
+                return default
+            raise ValueError('unable to parse unicode codepoint')
+
+        entries = [(c, as_hex_str(c, 'Internal error'), unicodedata.name(c, 'Name not found')) for c in characters]
+        page_source = CharInfoMenu(entries, per_page=1)
+        menu = NavMenuPages(page_source, delete_message_after=True)
+        await menu.start(ctx, wait=True)
+
+
+def setup(bot):
+    bot.add_cog(CharInfo(bot))
