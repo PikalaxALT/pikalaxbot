3697a726afb9041e4e8bab4a5545029aeaec8006
PikalaxALT <pikalaxalt@gmail.com>
2020-11-18 08:08:57 -0500

    Fix memberstatus maybe? 2

--- pikalaxbot/cogs/member_status.py
+++ pikalaxbot/cogs/member_status.py
@@ -61,9 +61,9 @@ class MemberStatus(BaseCog):
         await self.bot.send_tb(content)
 
     def do_plot_status_history(self, buffer, history):
-        times, values = zip(*history)
+        times, *values = zip(*history)
         plt.figure()
-        counts = {key: [v[key] for v in values] for key in self.colormap}
+        counts = {key: value for key, value in zip(discord.Status, values)}
         ax: plt.Axes = plt.gca()
         idxs = thin_points(len(times), 1000)
         for key, value in counts.items():
@@ -94,7 +94,7 @@ class MemberStatus(BaseCog):
             fetch_start = time.perf_counter()
             async with self.bot.sql as sql:
                 async with sql.execute('select timestamp, online, offline, dnd, idle from memberstatus where guild_id = ? and timestamp >= ? and timestamp < ? order by timestamp', (ctx.guild.id, hstart.timestamp(), hend.timestamp())) as cur:
-                    counts = [(datetime.datetime.fromtimestamp(row[0]), {name: count for name, count in zip(discord.Status, row[1:])}) async for row in cur]
+                    counts = await cur.fetchall()
             fetch_end = time.perf_counter()
             buffer = io.BytesIO()
             start = time.perf_counter()
