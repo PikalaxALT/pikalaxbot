b89773e8fd39d4dc4e2803b68e6e123147265218
PikalaxALT <pikalaxalt@gmail.com>
2020-11-18 07:57:34 -0500

    Fix mpl time axis maybe?

--- pikalaxbot/cogs/utils/mpl_time_axis.py
+++ pikalaxbot/cogs/utils/mpl_time_axis.py
@@ -9,6 +9,7 @@ __all__ = 'set_time_xlabs', 'thin_points'
 
 def set_time_xlabs(ax: plt.Axes, times: typing.List[datetime.datetime]):
     nticks = 6
+    times.sort()
     tick_width = (times[-1] - times[0]).total_seconds() / nticks
     new_ticks = [times[0] + datetime.timedelta(seconds=tick_width * i) for i in range(nticks + 1)]
     new_labs = [tstamp.strftime('%Y-%m-%d\nT%H:%M:%S') for tstamp in new_ticks]
