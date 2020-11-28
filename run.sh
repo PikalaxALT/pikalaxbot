8012d297ccc4d8316bafdb8f03c548f270553f8d
PikalaxALT <pikalaxalt@gmail.com>
2020-11-28 12:26:02 -0500

    No need to rebuild pokeapi every time

--- run.sh
+++ run.sh
@@ -6,7 +6,6 @@ BOTDIR=$(dirname "$(realpath -P "$0")"); cd "${BOTDIR}"
 # Update pokeapi
 git pull  # update the bot
 python3 -m pip install -U -r requirements.txt  # update discord
-if [ -f setup_pokeapi.sh ]; then ./setup_pokeapi.sh; fi
 JISHAKU_NO_UNDERSCORE=true \
 JISHAKU_NO_DM_TRACEBACK=true \
 python3 -m pikalaxbot  # start the bot
