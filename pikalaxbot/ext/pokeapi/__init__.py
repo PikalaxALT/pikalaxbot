6a6c27bab836879879236abbfb71cf27d7fd29d8
PikalaxALT <pikalaxalt@gmail.com>
2020-11-30 13:30:01 -0500

    Fix connector because factory kwarg is unusable

--- pikalaxbot/ext/pokeapi/__init__.py
+++ pikalaxbot/ext/pokeapi/__init__.py
@@ -1,6 +1,7 @@
 from .database import PokeApi
 import aiosqlite
 import os
+import sqlite3
 
 
 from .cog import *
@@ -14,7 +15,11 @@ def setup(bot):
     def factory():
         assert not cog._lock.locked(), 'PokeApi is locked'
         db_path = os.path.dirname(__file__) + '/../../../pokeapi/db.sqlite3'
-        return aiosqlite.connect(db_path, factory=PokeApi)
+
+        def connector():
+            return sqlite3.connect(db_path)
+
+        return PokeApi(connector, 64)
 
     bot.pokeapi = factory
     bot.add_cog(cog)
