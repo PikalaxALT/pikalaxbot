e264700f184fe28ea3fb5e2b2c8fe4c38b01f57d
PikalaxALT <pikalaxalt@gmail.com>
2020-12-25 15:12:30 -0500

    Connect to pokeapi as read-only

--- pikalaxbot/ext/pokeapi/__init__.py
+++ pikalaxbot/ext/pokeapi/__init__.py
@@ -11,10 +11,10 @@ from .database import *
 def setup(bot):
     cog = PokeApiCog(bot)
 
-    db_path = os.path.join(os.path.dirname(__dirname__), 'pokeapi', 'db.sqlite3')
+    db_path = os.path.join('file:' + os.path.dirname(__dirname__), 'pokeapi', 'db.sqlite3?mode=ro')
 
     def connector():
-        return sqlite3.connect(db_path, factory=PokeApiConnection)
+        return sqlite3.connect(db_path, factory=PokeApiConnection, uri=True)
 
     conn = PokeApi(connector, 64)
 
