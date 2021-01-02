a0327161db4721cd63e15106368a996256ae9ae6
PikalaxALT <pikalaxalt@gmail.com>
2021-01-02 10:39:24 -0500

    Refactor seen_user

--- pikalaxbot/cogs/seen_user.py
+++ pikalaxbot/cogs/seen_user.py
@@ -1,19 +1,13 @@
 import datetime
 import discord
-from collections import defaultdict, namedtuple
+from collections import defaultdict
 from discord.ext import commands
 import typing
 from . import BaseCog
 from humanize import naturaldelta
 import re
-
-
-def get_msg_reference(message):
-    return namedtuple('MessageReference', 'message_id channel_id guild_id')(message.id, message.channel.id, message.guild.id)
-
-
-def get_jump_url(ref):
-    return f'https://discordapp.com/channels/{ref.guild_id}/{ref.channel_id}/{ref.message_id}'
+if typing.TYPE_CHECKING:
+    from .. import PikalaxBOT
 
 
 class SeenUser(BaseCog):
@@ -23,45 +17,33 @@ class SeenUser(BaseCog):
 
     def __init__(self, bot):
         super().__init__(bot)
-        self.member_cache = {}
-        self.history_cache = defaultdict(list)
+        self.member_cache: dict[tuple[discord.Guild, discord.Member], discord.Message] = {}
+        self.history_cache: dict[discord.TextChannel, list[discord.Message]] = defaultdict(list)
 
     @BaseCog.listener()
     async def on_message(self, message: discord.Message):
         if message.guild is not None:
-            ref = get_msg_reference(message)
-            self.member_cache[(message.guild.id, message.author.id)] = ref
-            self.history_cache[message.channel.id].append((message.author.id, ref))
+            self.member_cache[(message.guild, message.author)] = message
+            self.history_cache[message.channel].append(message)
 
-    async def get_last_seen_msg(self, member: discord.Member) -> typing.Optional[discord.MessageReference]:
+    async def get_last_seen_msg(self, member: discord.Member) -> typing.Optional[discord.Message]:
         last = datetime.datetime.utcnow() - SeenUser.MAX_LOOKBACK
-        seen_msg: typing.Optional[discord.MessageReference] = None
+        seen_msg: typing.Optional[discord.Message] = None
         for channel in member.guild.text_channels:  # type: discord.TextChannel
-            if channel.id in self.history_cache:
-                history = self.history_cache[channel.id]
-            elif channel.permissions_for(member.guild.me).read_message_history:
-                history = [(message.author.id, get_msg_reference(message)) async for message in channel.history(limit=None, after=last)]
-                if history:
-                    history.sort(key=lambda msg: discord.utils.snowflake_time(msg[1].message_id))
-                self.history_cache[channel.id] = history
-            else:
-                continue
-            x = discord.utils.find(lambda m: m[0] == member.id, reversed(history))
-            if x is not None:
-                seen_msg = x[1]
-                last = discord.utils.snowflake_time(seen_msg.message_id)
+            if (history := self.history_cache.get(channel)) is None:
+                if not channel.permissions_for(member.guild.me).read_message_history:
+                    continue
+                self.history_cache[channel] = history = sorted(await channel.history(limit=None, after=last).flatten(), key=lambda msg: msg.created_at)
+            last = getattr((seen_msg := discord.utils.get(reversed(history), author=member) or seen_msg), 'created_at', last)
         return seen_msg
 
     @commands.command()
     async def seen(self, ctx: commands.Context, *, member: discord.Member):
         """Returns the last message sent by the given member in the current server.
         Initially looks back up to 24 hours."""
-        key = (ctx.guild.id, member.id)
-        if key in self.member_cache:
-            seen_msg = self.member_cache[key]
-        else:
-            seen_msg = await self.get_last_seen_msg(member)
-            self.member_cache[key] = seen_msg
+        key = (ctx.guild, member)
+        if (seen_msg := self.member_cache.get(key)) is None:
+            self.member_cache[key] = seen_msg = await self.get_last_seen_msg(member)
         if seen_msg is None:
             ndelt = naturaldelta(SeenUser.MAX_LOOKBACK)
             # 1 day is parsed to "a day" but that's bad grammar here
@@ -69,9 +51,9 @@ class SeenUser(BaseCog):
             await ctx.send(f'{member.display_name} has not said anything on this server in the last {ndelt}.')
         else:
             await ctx.send(f'{member.display_name} was last seen chatting in <#{seen_msg.channel_id}> '
-                           f'{discord.utils.snowflake_time(seen_msg.message_id).strftime("on %d %B %Y at %H:%M:%S UTC")}\n'
-                           f'{get_jump_url(seen_msg)}')
+                           f'{seen_msg.created_at.strftime("on %d %B %Y at %H:%M:%S UTC")}\n'
+                           f'{seen_msg.jump_url}')
 
 
-def setup(bot):
+def setup(bot: 'PikalaxBOT'):
     bot.add_cog(SeenUser(bot))
