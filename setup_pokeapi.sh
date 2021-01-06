8c7db04f64585389f20735d69004a4a98ee4dab4
PikalaxALT <pikalaxalt@gmail.com>
2021-01-06 17:24:18 -0500

    Bump PokeAPI

--- setup_pokeapi.sh
+++ setup_pokeapi.sh
@@ -8,13 +8,13 @@ if ! [ -d pokeapi ]; then
   git submodule update
   git submodule foreach --recursive init
 fi
-git submodule update --recursive
+git submodule update --recursive --remote
 cd pokeapi
 # shellcheck disable=SC2046
 python3 -m pip install -U $(grep -v psycopg2 requirements.txt) psycopg2
 newfiles=$(find data/v2/csv -name "*.csv" -newer db.sqlite3 || ls data/v2/csv/*.csv)
 make setup
-if ! [ -z "$newfiles" ]; then
+if [ -n "$newfiles" ]; then
   python3 manage.py shell -c "from data.v2.build import build_all; build_all()" --settings=config.local
 fi
 
