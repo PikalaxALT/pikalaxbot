f22ed2b50bbd6669b35acb5372adccb158e760e7
PikalaxALT <pikalaxalt@gmail.com>
2021-01-16 15:12:04 -0500

    Cleanup setup_pokeapi.sh

--- setup_pokeapi.sh
+++ setup_pokeapi.sh
@@ -12,7 +12,7 @@ git submodule update --recursive --remote
 cd pokeapi
 # shellcheck disable=SC2046
 python3 -m pip install -U $(grep -v psycopg2 requirements.txt) psycopg2
-newfiles=$(find data/v2/csv -name "*.csv" -newer db.sqlite3 || ls data/v2/csv/*.csv)
+newfiles=$(find data/v2/csv -name "*.csv" -newer db.sqlite3 2> /dev/null || ls data/v2/csv/*.csv)
 make setup
 if [ -n "$newfiles" ]; then
   python3 manage.py shell -c "from data.v2.build import build_all; build_all()" --settings=config.local
